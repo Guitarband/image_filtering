c7839028ef5bb0bcc1ec10fa680ffc9141bcc86e
Preetish Choudhary <60247328+Guitarband@users.noreply.github.com>
2021-06-10 16:02:10 +1000

    Update imageFilter.js

--- imageFilter.js
+++ imageFilter.js
@@ -1,19 +1,10 @@
-function displayImage(input) {
-        if (input.files && input.files[0]) {
-                var reader = new FileReader();
-                var width = imageElement.naturalWidth;
-                var height = imageElement.naturalHeight;
-
-            reader.onload = function (e) {
-                    document.getElementById("uploadedImage").src = 'e.target.result';
-                $('#uploadedImage')
-                        .attr('src', e.target.result)
-                        .width(400);
-                        .height(400);
-            };
-
-            reader.readAsDataURL(input.files[0]);
-        }
+function showPreviewOne(event){
+      if(event.target.files.length > 0){
+        let src = URL.createObjectURL(event.target.files[0]);
+        let preview = document.getElementById("uploadedImage");
+        preview.src = src;
+        preview.style.display = "block";
+      } 
     }
 
 
