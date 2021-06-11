26fcdf44e19096b47b7b261c998fdf72a5a09078
Preetish Choudhary <60247328+Guitarband@users.noreply.github.com>
2021-06-11 22:14:13 +1000

    Update imageFilter.js

--- imageFilter.js
+++ imageFilter.js
@@ -4,17 +4,16 @@ function showPreviewOne(event){
         let preview = document.getElementById("uploadedImage");
         preview.src = src;
         preview.style.display = "block";
-            
-            var canvas, context;
 
-            canvas = document.getElementById('cv');
-            context = canvas.getContext('2d');
-            context.drawImage(src', 10, 30, 20, 20);
       } 
     }
 
 
+var canvas, context;
 
+canvas = document.getElementById('cv');
+context = canvas.getContext('2d');
+context.drawImage('test.jpg', 10, 30, 20, 20);
 
 
 //ImageBitmap.height()
